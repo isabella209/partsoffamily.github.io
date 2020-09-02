74b4e1866c7c977800b2ea0e27f893fc0d4c1225
AYESHA <humairamirfath@gmail.com>
2020-09-02 20:29:29 +0600

    familypls

---
+++ sqript.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	$(".nature").click(function(){
+		$("#nature").slideToggle(2000);
+	});
+});
+
+
+$(document).ready(function(){
+	$(".stars").click(function(){
+		$("#stars").slideToggle(2000);
+	});
+});
+
+
+
+$(document).ready(function(){
+	$(".rainbow").click(function(){
+		$("#rainbow").slideToggle(3000);
+	});
+});
+
+
+$(document).ready(function(){
+	$(".trees").click(function(){
+		$("#trees").slideToggle(3000);
+	});
+});
+
+
+$(document).ready(function(){
+	$(".Grandparent_Family").click(function(){
+		$("#Grandparent_Family").slideToggle(3000);
+	});
+});
+
+
+$(document).ready(function(){
+	$(".step").click(function(){
+		$("#step").slideToggle(3000);
+	});
+});
